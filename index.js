30298dfb64136aa8c0980be9b40d43e8c9ef974b
Mimi Majgaard <mimimajgaard@gmail.com>
2022-02-24 15:19:47 -0800

    commit tree 3c92e382162e457d4d0d1885d4fbd20f2bb6d15e

---
+++ index.js
@@ -0,0 +1,39 @@
+
+
+function add(a,b) {
+return a + b; 
+}
+
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return (a * b)
+}
+
+
+function divide(a, b) {
+    return (a / b)
+}
+
+
+
+function increment(a) {
+    return number = (a+1);
+}
+
+
+function decrement(a) {
+    return number = (a-1);
+}
+
+
+
+function makeInt(a) { 
+console.log("a", a);
+return n = a;
+}
+
+
