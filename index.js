3dc7fc1460ae702a09d69fb86120f925d7253676
Mimi Majgaard <mimimajgaard@gmail.com>
2022-02-26 11:11:21 -0800

    Completed assignment

---
+++ index.js
@@ -0,0 +1,49 @@
+
+
+function add(a,b) {
+return a + b; 
+}
+
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return (a * b)
+}
+
+
+function divide(a, b) {
+    return (a / b)
+}
+
+
+
+function increment(a) {
+    return (a+1);
+}
+
+
+function decrement(a) {
+    return (a-1);
+}
+
+
+
+function makeInt(a) { 
+console.log("a", a);
+
+}
+
+
+function makeInt(a) {
+    parseInt("a", 10);
+    return a;
+}
+
+
+function preserveDecimal(a) {
+    parseFloat("a");
+    return "a";
+}
